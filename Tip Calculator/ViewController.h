7f5eabe6a3c2e99eef139d03802939f851e54a92
Yam Lin <yamlin@yahoo-inc.com>
2015-06-11 17:23:47 +0800

    Initial Commit

---
+++ Tip Calculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tip Calculator
+//
+//  Created by Jhih-Yan Lin on 6/11/15.
+//  Copyright (c) 2015 Jhih-Yan Lin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Tip Calculator/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Tip Calculator
+//
+//  Created by Jhih-Yan Lin on 6/11/15.
+//  Copyright (c) 2015 Jhih-Yan Lin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
